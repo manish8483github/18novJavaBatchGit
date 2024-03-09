2e3f7500105f530b0e0b707e2783e04ab909b77b
Your Name <you@example.com>
2024-03-09 13:29:28 +0530

    added arraylist code for different city

---
+++ ArrayList3/src/Array1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		List<String> city = new ArrayList<String>();
+		city.add("mumbai");
+		city.add("punjab");
+		city.add("delhi");
+		System.out.println(city);
+	}
+}
