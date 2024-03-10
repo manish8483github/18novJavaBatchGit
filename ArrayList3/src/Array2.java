e796ddeb4212d122b5fb16a7f4ed69d8c0c40732
manish <mmanish9090@gmail.com>
2024-03-10 21:24:24 +0530

    added arraylist code number and city

---
+++ ArrayList3/src/Array2.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList<>();
+		al.add("pune");
+		al.add("mumbai");
+		al.add("sangali");
+		System.out.println(al);
+	}
+}
